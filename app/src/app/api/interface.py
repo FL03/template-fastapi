4125513bea72ad6d27baf9c3804b3de20827fa4d
FL03 <jo3mccain@icloud.com>
2022-09-21 07:23:34 -0500

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- app/src/app/api/interface.py
+++ app/src/app/api/interface.py
@@ -8,5 +8,6 @@ from fastapi import APIRouter
 from app.api.endpoints import auth, users
 
 router = APIRouter(tags=['v1'])
+
 router.include_router(router=auth.router)
 router.include_router(router=users.router)
